2d7c75800ff6bf0eccd542cac6de55ec7482b178
RayenBSD <92104474+RayenBSD@users.noreply.github.com>
2023-01-18 09:16:42 +0100

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,25 @@
+import { fonts } from "./fonts";
+import { curtisMacnewton, liamMckay, mosaMoseneke, copy1, copy2, copy3, copy9, copy11, copy12, copy13, rawpixel } from './images';
+import { profiles, matchScores } from './data';
+
+export {
+    // Data
+    profiles,
+    matchScores,
+
+    // Fonts
+    fonts,
+
+    // Images
+    curtisMacnewton,
+    liamMckay,
+    mosaMoseneke,
+    copy1,
+    copy2,
+    copy3,
+    copy9,
+    copy11,
+    copy12,
+    copy13,
+    rawpixel,
+}
